812d7ba14b80725b97709784908addad4e06f43a
Rasik Pandit <Rpandit3867@conestogac.on.ca>
2025-10-30 19:12:59 -0400

    Updated user model to include UserID and prepare for JWT integration

--- Group5F25.API/Models/User.cs
+++ Group5F25.API/Models/User.cs
@@ -1,6 +1,12 @@
-﻿namespace Group_5_Project_Ayman_Birendra_Cole_Rasik.API.Models
+﻿namespace Group5F25.API.Models
 {
     public class User
     {
+        public int UserId { get; set; }                 // PK
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty; // unique
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
