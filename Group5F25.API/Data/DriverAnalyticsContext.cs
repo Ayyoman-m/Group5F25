f650314dd9bc933f97534cef45cc723d5a679142
Rasik Pandit <Rpandit3867@conestogac.on.ca>
2025-10-30 22:32:52 -0400

    Added DataAnalyticsContect and EF configuration AB #52

--- Group5F25.API/Data/DriverAnalyticsContext.cs
+++ Group5F25.API/Data/DriverAnalyticsContext.cs
@@ -1,6 +1,24 @@
-﻿namespace Group_5_Project_Ayman_Birendra_Cole_Rasik.API.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Group5F25.API.Models;
+
+namespace Group5F25.API.Data
 {
-    public class DriverAnalyticsContext
+    // This class connects the app to the database using Entity Framework Core
+    public class DriverAnalyticsContext : DbContext
     {
+        // Constructor passes options (like connection string) to the base DbContext
+        public DriverAnalyticsContext(DbContextOptions<DriverAnalyticsContext> options) : base(options) { }
+
+        // Table for storing user records in the database
+        public DbSet<User> Users => Set<User>();
+
+        // This method sets up special rules for the database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Make sure each user has a unique email (no duplicates allowed)
+            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
