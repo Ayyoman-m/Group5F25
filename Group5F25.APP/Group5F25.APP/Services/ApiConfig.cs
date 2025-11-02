dc81f8ef2c92c8933f6c40a8a784c4045ce095ed
Bdeuba7977 <bdeuba7977@conestogac.on.ca>
2025-11-02 00:25:15 -0400

    add ApiConfig for DummyJSON endpoints AB#11

---
+++ Group5F25.APP/Group5F25.APP/Services/ApiConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Group5F25.APP
+{
+    public static class ApiConfig
+    {
+        public const string BaseUrl = "https://dummyjson.com/";
+        public const string LoginPath = "auth/login";
+        public const string MePath = "auth/me";
+    }
+}
